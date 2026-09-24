3ea3ea417b7dc5ce6981c3e9e6b81d9a64f98d22
Md. Maruf Hasan Tuhin <marufh2894@gmail.com>
2026-09-24 20:10:11 +0600

    75. Sort Colors

---
+++ Array/75. Sort Colors.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        l,mid,h=0,0,len(nums)-1
+        while mid<=h:
+            if nums[mid]==0:
+                nums[l],nums[mid]=nums[mid],nums[l]
+                l+=1
+                mid+=1
+            elif nums[mid]==1:
+                mid+=1
+            else:
+                nums[mid],nums[h]=nums[h],nums[mid]
+                h-=1
